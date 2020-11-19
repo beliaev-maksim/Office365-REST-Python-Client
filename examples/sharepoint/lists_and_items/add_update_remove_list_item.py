eabaa323730a59ef9bd887a8f50e5acb3c819103
Maksim Beliaev <--global>
2020-11-19 13:41:44 +0100

    added explicit comments that items will be unrecoverable deleted

--- examples/sharepoint/lists_and_items/add_update_remove_list_item.py
+++ examples/sharepoint/lists_and_items/add_update_remove_list_item.py
@@ -29,22 +29,23 @@ item_to_update.update()
 ctx.execute_query()
 
 
-# removing item. Remove when item property meets one of the conditions Title or version, version might be either 2 or 3
+# Move item to recycle bin.
+# Remove when item property meets one of the conditions Title or version, version might be either 2 or 3
 target_list.recycle_item_by_property({"Title": "my_title",
                                       "version": ["2", "3"]}, or_filter=True)
 
-# remove item when both conditions meet. Title is my_title and build_date is either "2020-11-17" or "2020-11-18"
+# Recycle when both conditions meet. Title is my_title and build_date is either "2020-11-17" or "2020-11-18"
 target_list.recycle_item_by_property({"Title": "my_title",
                                       "build_date": ["2020-11-17", "2020-11-18"]}, or_filter=False)
 ctx.execute_query()
 
 
-# delete item by ID
+# delete item by ID (will completely delete item)
 target_item = target_list.get_item_by_id(54)
 target_item.delete_object().execute_query()
 
 
-# delete all items
+# delete all items (will completely delete items)
 result = target_list.items.get().execute_query()  # get existing items
 for item in result:
     item.delete_object()
